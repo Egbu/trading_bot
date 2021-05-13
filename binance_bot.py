6599a7f1531d6aa38df229aebb47e7364c320deb
Egbu <eugwumsi@gmail.com>
2021-05-13 01:51:14 +0100

    :zap::snake: Feat: Used env vars

--- binance_bot.py
+++ binance_bot.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 import datetime as dt 
 from binance.client import Client 
@@ -8,8 +9,9 @@ import requests
 
 
 
-api_key='clEiLOWrdR5Ay7dxPnR2IYKKKNpy4C7W25t26hLuObr4FYc9Y1HeuJViWAhUua1v'
-api_secret='S0SubR3o4AsnAFajLObIILgqAfKc8KxUGvbNL3YIzQrqyFuXMj5kS0gi6RmU5cyl'
+
+api_key= os.getenv("api_key")
+api_secret= os.environ("api_secret")
 
 client = Client(api_key, api_secret)
 
@@ -38,22 +40,31 @@ D_high = D[['high']]
 # index = D_high_close.index
 
 
-
-
-for x in D_high_close['close']:
+def five_percent_increase():
+  for x in D_high_close['close']:
     #print(x, 'This is the value of x')
     num = (float(x) * 0.05) + float(x)
-    print(num)
+    print(num, "I printed it")
+    
 
     #  if i == 0:
     #      percentage(num)
         
     #      i += 1
     #      print(percentage(num))
-    
 
-for i in D_high:
-  print(D_high )
+
+#five_percent_increase()
+
+compare_values = pd.DataFrame(D_high, five_percent_increase())
+
+# compare_values.columns = ['HIGH', 'FIVE_PERCENT_INCREASE']
+
+print(compare_values)
+
+
+#for i in D_high:
+#  print(D_high )
 #print (D_close)
 
 
